c317a234022dfea1fd8253d48cc259d940e420e3
1C0D <1conscience@gmx.fr>
2023-12-19 00:45:48 +0100

    mjs modif

--- scripts/update-version.mjs
+++ scripts/update-version.mjs
@@ -2,6 +2,7 @@ import readline from 'readline';
 import { execSync } from 'child_process';
 import { readFileSync, writeFileSync } from "fs";
 import dedent from 'dedent';
+import semver from 'semver';
 
 function updateVersion() {
     const rl = readline.createInterface({
@@ -15,58 +16,60 @@ function updateVersion() {
         minor(1.1.0) -> type 2 or min
         major(2.0.0) -> type 3 or maj
     \n`, (updateType) => {
-            rl.close();
+        rl.close();
 
         // Increment version for chosen type
         const currentVersion = process.env.npm_package_version;
-        let [major, minor, patch] = currentVersion.split('.').map(Number);
-        updateType = updateType.trim()
+        let targetVersion;
 
-        if (updateType === 'p' || updateType === '1') {
-            patch++;
-        } else if (updateType === 'min' || updateType === '2') {
-            minor++;
-            patch = 0;
-        } else if (updateType === 'maj' || updateType === '3') {
-            major++;
-            minor = 0;
-            patch = 0;
-        } else {
-            console.log("wrong type")
-            process.exit(1);
+        switch (updateType.trim()) {
+            case 'p':
+            case '1':
+                targetVersion = semver.inc(currentVersion, 'patch');
+                break;
+            case 'min':
+            case '2':
+                targetVersion = semver.inc(currentVersion, 'minor');
+                break;
+            case 'maj':
+            case '3':
+                targetVersion = semver.inc(currentVersion, 'major');
+                break;
+            default:
+                console.log("wrong type");
+                process.exit(1);
         }
 
-        const targetVersion = `${major}.${minor}.${patch}`;
-
-        updateManifestVersions(targetVersion)
+        updateManifestVersions(targetVersion);
 
         // Git add, commit et push
         execSync(`git add -A && git commit -m "Updated to version ${targetVersion}" && git push`);
-        console.log(`version update to ${targetVersion}`);
+        console.log(`version updated to ${targetVersion}`);
+        process.exit();
     });
 }
 
 function updateManifestVersions(targetVersion) {
-    // read minAppVersion from manifest.json and bump version to target version
+    // Read minAppVersion from manifest.json and bump version to target version
     let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
     const { minAppVersion } = manifest;
     manifest.version = targetVersion;
     writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-    // update versions.json with target version and minAppVersion from manifest.json
+    // Update versions.json with target version and minAppVersion from manifest.json
     let versions = JSON.parse(readFileSync("versions.json", "utf8"));
     versions[targetVersion] = minAppVersion;
     writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
-    
-    // update package.json
+
+    // Update package.json
     let packageJsn = JSON.parse(readFileSync("package.json", "utf8"));
     packageJsn.version = targetVersion;
     writeFileSync("package.json", JSON.stringify(packageJsn, null, "\t"));
 
-    // this is doing a bug
-    // let packageLockJsn = JSON.parse(readFileSync("package-lock.json", "utf8"));
-    // packageLockJsn.version = targetVersion;
-    // writeFileSync("package.json", JSON.stringify(packageLockJsn, null, "\t"));
+    // Update package-lock.json
+    let packageLockJsn = JSON.parse(readFileSync("package-lock.json", "utf8"));
+    packageLockJsn.version = targetVersion;
+    writeFileSync("package-lock.json", JSON.stringify(packageLockJsn, null, "\t"));
 }
 
-updateVersion()
+updateVersion();
